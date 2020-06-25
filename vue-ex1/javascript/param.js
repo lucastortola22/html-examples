5189377097aa5993a95b51c139b2329c5e192d42
Lucas Tortola <lucastortola@gmail.com>
2020-06-25 23:27:03 +1000

    vue examples

---
+++ vue-ex1/javascript/param.js
@@ -0,0 +1,20 @@
+function soma(a, b, c){
+    a = a || 1
+    b = b || 1
+    c = c || 1
+
+    return a + b + c
+}
+
+console.log(soma(), soma(3), soma(0,0,0));
+
+function soma2(a, b , c){
+    a = a !== undefined ? a : 1
+    b = 1 in arguments ? b : 1
+    c = isNaN(c) ? 1 : c
+
+    return a + b + c
+}
+
+
+console.log(soma2(), soma2(3), soma2(0,0,0));
